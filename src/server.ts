b9afcac3c594d4664c1be745e048ae6a37d0a1b2
Eugene Bakaev <eugene.bakaev@gmail.com>
2026-05-16 12:26:36 +0300

    Add eslint with ts support - Configure eslint with typescript-eslint recommended rules - Add linting into build command

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const env = loadEnv();
 const app = createApp();
 
 app.listen(env.PORT, () => {
-  console.log(`API listening on http://localhost:${env.PORT}`);
+  console.log(`API listening on http://localhost:${env.PORT}`); // eslint-disable-line no-console
 });
